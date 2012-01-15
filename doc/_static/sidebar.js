58c27ea5c3cfda2b5a1eb53f41af34df95e6ff02
Stephen Sugden <glurgle@gmail.com>
2012-01-15 00:31:39 -0800

    Tweaked the sidebar highlighting on doc page

--- doc/_static/sidebar.js
+++ doc/_static/sidebar.js
@@ -1,41 +1,52 @@
 $(document).ready(function() {
-    /* jQuery .offset appears to not work correctly in firefox */
-    function yOffset(elem) {
-        offset = 0
-        do {
-            offset += elem.offsetTop
-        } while(elem = elem.offsetParent)
-        return offset
-    }
-    
-    var sections = $('div.section').get().map(function (node) {
-        node.yTop = yOffset(node)
-        var header = $(node).children('h1, h2').first()
-        node.yBottom = yOffset(header.get(0)) + header.outerHeight()
-        return node
-    })
+  function yOffset(elem) {
+    offset = 0
+    do {
+      offset += elem.offsetTop
+    } while(elem = elem.offsetParent)
+    return offset
+  }
+  
+  var sectionNames = {}
+  var sections = $('div.section').get().map(function (node) {
+    node.yTop = yOffset(node)
+    var header = $(node).children('h1, h2').first()
+    node.yBottom = yOffset(header.get(0)) + header.outerHeight()
+    sectionNames[node.id] = node
+    return node
+  })
 
-    function currentSection(section) {
-        var w = $(window)
-        var top = w.scrollTop()
-        var bottom = w.scrollTop() + w.height()
-        var half = top + ((bottom - top) * 0.66)
-        var section = sections.filter(function (section) {
-            return section.yBottom >= top && section.yTop < half
-        })[0]
+  function currentSection() {
+    var w = $(window)
+    var top = w.scrollTop()
+    var bottom = w.scrollTop() + w.height()
+    var half = top + ((bottom - top) * 0.66)
+    var section = sections.filter(function (section) {
+      return section.yBottom >= top && section.yTop < half
+    })[0]
 
-        if (!section || $(section).hasClass('currentsection')) return
+    if (!section || $(section).hasClass('currentsection')) return
 
-        $(".currentsection").removeClass('currentsection')
-        $(section).addClass('currentsection')
-        $(".sphinxsidebar a[href='#" + section.id + "']")
-            .parent().addClass('currentsection')
-    }
+    highlightSection(section)
+  }
 
-    $(window).scroll(currentSection)
-    $('.sphinxsidebar a.internal.reference').click(function() {
-        var id = this.href.split('#').pop()
-        currentSection(document.getElementById(id))
-    })
+  function highlightSection(section) {
+    $(".currentsection").removeClass('currentsection')
+    $(section).addClass('currentsection')
+    $(".sphinxsidebar a[href='#" + section.id + "']")
+      .parent().addClass('currentsection')
+  }
+
+  $('.sphinxsidebar a.internal.reference').click(function() {
+    var id = this.href.split('#').pop()
+    highlightSection(document.getElementById(id))
+  })
+
+  $(window).scroll(currentSection)
+  var hash;
+  if (hash = document.location.hash) {
+    highlightSection(sectionNames[hash.substring(1)])
+  } else {
     currentSection()
+  }
 })
